1c4b4ca6cd9882c1a4609d1ca83c6e72d23f5d91
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-01-15 11:53:28 +0000

    AST structure implementation

---
+++ source/ast/ast.d
@@ -0,0 +1,93 @@
+module ast.ast;
+
+import token.token;
+
+/+++/
+interface Node {
+
+    /+++/
+    string tokenLiteral();
+}
+
+/+++/
+interface Statement : Node {
+
+    /+++/
+    void statementNode();
+}
+
+
+/+++/
+interface Expression : Node {
+
+    /+++/
+    void expressionNode();
+}
+
+/+++/
+class Program {
+    Statement[] statements; /// a program is a bunch of statements
+
+    /***********************************
+     * foo does this.
+     * Params:
+     *      stmts =     an array of Statements
+     */
+    this() {
+        this.statements = [];
+    }
+
+    // this(this) {
+    //     statements = statements.dup;
+    // }
+
+    /+++/
+    string tokenLiteral() {
+        if(this.statements.length > 0)
+            return this.statements[0].tokenLiteral();
+        return "";
+    }
+}
+
+/+++/
+struct Identifier {
+    Token token;    /// the IDENTIFIER token type
+    string value;   /// the value (name) if this identifier.
+
+    /***********************************
+     * expressionNode does nothing in particular.
+     */
+    void expressionNode()  {}
+
+    /+++/
+    string tokenLiteral() {
+        return this.token.literal;
+    }
+}
+
+
+/+++/
+class LetStatement : Statement {
+    Token token;      /// the LET token type.
+    Expression value; ///  the expression that produces a value to be bound
+    Identifier name;  /// idenifier for the bound
+
+    /***********************************
+     * Constructor
+     */
+    this(Token token) {
+        this.token = token;
+    }
+
+    /***********************************
+     * statementNode does nothing in particular. 
+     * mostly used for debugging purposes.
+     */
+    void statementNode() {}
+
+    /+++/
+    string tokenLiteral() {
+        return this.token.literal;
+    }
+}
+
