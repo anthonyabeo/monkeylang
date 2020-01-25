a85fd1dde1ab876879e5afbe54bba85427881ad8
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-01-25 23:29:36 +0000

    enviroments can access their their outer scope environments.

--- source/objekt/environment.d
+++ source/objekt/environment.d
@@ -3,8 +3,9 @@ module objekt.environment;
 import objekt.objekt;
 
 ///
-struct Environment {
+class Environment {
     Objekt[string] store;   /// store
+    Environment outer;      /// enclosing scope environment
 
     ///
     this(Objekt[string] store) {
@@ -12,11 +13,21 @@ struct Environment {
     }
 
     ///
-    Objekt get(string name) {
-        if(name in this.store)
-            return this.store[name];
+    static newEnclosingEnvironment(Environment outer) {
+        auto env = new Environment((Objekt[string]).init);
+        env.outer = outer;
 
-        return null;
+        return env;
+    }
+
+    ///
+    Objekt get(string name) {
+        Objekt obj = this.store.get(name, null);
+
+        if(obj is null && (this.outer !is null))
+            obj = this.outer.get(name);
+        
+        return obj;
     }
 
     ///
