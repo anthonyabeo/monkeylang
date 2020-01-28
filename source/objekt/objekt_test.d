3a163c20942bea7583f25dacf80c005e45a50c2f
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-01-28 15:54:24 +0000

    test hash key methods

---
+++ source/objekt/objekt_test.d
@@ -0,0 +1,31 @@
+module objekt.objekt_test;
+
+import std.stdio;
+
+import objekt.objekt;
+
+unittest {
+    testStringHashKey();
+}
+
+void testStringHashKey() {
+    auto hello1 = new String("Hello World");
+    auto hello2 = new String("Hello World");
+    auto diff1 = new String("My name is johnny");
+    auto diff2 = new String("My name is johnny");
+
+    if(hello1.hashKey() != hello2.hashKey()) {
+        stderr.writeln("strings with same content have different hash keys");
+        assert(hello1.hashKey() == hello2.hashKey());
+    }
+
+    if(diff1.hashKey() != diff2.hashKey()) {
+        stderr.writeln("strings with same content have different hash keys");
+        assert(diff1.hashKey() == diff2.hashKey());
+    }
+
+    if(hello1.toHash() == diff1.toHash()) {
+        stderr.writeln("strings with different content have same hash keys");
+        assert(hello1.hashKey() != diff1.hashKey());
+    }
+}
