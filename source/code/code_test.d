ee6159cd8347808b0b81ff07107869428b94e32c
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-01-31 18:23:08 +0000

    test for creating an bytecode instruction

---
+++ source/code/code_test.d
@@ -0,0 +1,37 @@
+module code.code_test;
+
+import std.stdio;
+import std.typecons;
+
+import code.code;
+
+
+unittest {
+    testMake();
+}
+
+/+++/
+void testMake() {
+    alias Instr = Tuple!(OPCODE, "op", 
+                         int[], "operands", 
+                         ubyte[], "expected");
+
+    auto tests = [
+        Instr(OPCODE.OpConstant, [65534], [cast(ubyte) OPCODE.OpConstant, 255, 254]),
+    ];
+
+    foreach(tt; tests) {
+        auto instruction = make(tt.op, tt.operands);
+        if(instruction.length != tt.expected.length) {
+            stderr.writefln("instruction has wrong length. want=%d, got=%d", tt.expected.length, instruction.length);
+            assert(instruction.length == tt.expected.length);
+        }
+
+        foreach(i, b; tt.expected) {
+            if(instruction[i] != tt.expected[i]) {
+                stderr.writeln("wrong byte at pos %d. want=%d, got=%d", i, b, instruction[i]);
+                assert(instruction[i] == tt.expected[i]);
+            }
+        }
+    }
+}
