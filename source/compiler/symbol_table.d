e083633fae9d3f0a545b94f6fac93006ba88501e
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-02-06 12:15:09 +0000

    symbol table with define and resolve methods

---
+++ source/compiler/symbol_table.d
@@ -0,0 +1,47 @@
+module compiler.symbol_table;
+
+import std.typecons;
+
+
+///
+enum SymbolScope {
+    GLOBAL,
+}
+
+///
+struct Symbol {
+    string name;            /// name
+    SymbolScope skope;      /// scope
+    size_t index;           /// index
+}
+
+///
+struct SymbolTable {
+    Symbol[string] store;   /// store
+    size_t numDefinitions;  /// number of definitions
+    
+    /++
+     + Define
+     +
+     + Params
+     +      name=
+     +
+     + Return
+     +      Symbol
+     +/
+    Symbol define(string name) {
+        auto symbol = Symbol(name, SymbolScope.GLOBAL, this.numDefinitions);
+        this.store[name] = symbol;
+        this.numDefinitions++;
+
+        return symbol;
+    }
+
+    ///
+    Nullable!Symbol resolve(string name) {
+        if(name !in this.store)
+            return Nullable!Symbol();
+
+        return Nullable!Symbol(this.store[name]);
+    }
+}
